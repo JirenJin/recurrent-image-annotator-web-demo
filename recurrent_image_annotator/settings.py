3e4af56fa527f0d8c5ff53d3f805e20736c93a82
Jiren Jin <jinjiren@foxmail.com>
2016-07-10 17:31:52 +0900

    fix: rename 'annotation' app to 'annotator', to improve clarity, since the app has a model also named annotation. And the new name is more likely to represent the action attribute of the app.

--- recurrent_image_annotator/settings.py
+++ recurrent_image_annotator/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'annotation.apps.AnnotationConfig',
+    'annotator.apps.AnnotatorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
