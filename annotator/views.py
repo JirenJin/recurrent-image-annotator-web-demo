144b8d5e813f833fd7c1a85c947bc0625ae336c3
Jiren Jin <jinjiren@foxmail.com>
2016-07-10 23:34:37 +0900

    feat: Set basic layout of index.html

--- annotator/views.py
+++ annotator/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the annotation index.")
+    return render(request, 'annotator/index.html')
