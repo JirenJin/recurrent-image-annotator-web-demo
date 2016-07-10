61847ca949723e41bd52b9c4b27a049927493eda
Jiren Jin <jinjiren@foxmail.com>
2016-07-10 12:23:06 +0900

    feat: add 'annotation' app in settings.py

--- recurrent_image_annotator/settings.py
+++ recurrent_image_annotator/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'annotation.apps.AnnotationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
