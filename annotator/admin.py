e9862fa887069b6f4d045fe4809ac02495d13c8a
Jiren Jin <jinjiren@foxmail.com>
2016-07-10 23:33:41 +0900

    feat: Add annotator app's models to admin.py

--- annotator/admin.py
+++ annotator/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Image, Label, Annotation, Assign
+
+admin.site.register(Image)
+admin.site.register(Label)
+admin.site.register(Annotation)
+admin.site.register(Assign)
