7db8e9de71d036787355ea3749a19e5d5d8ea9a1
Jiren Jin <jinjiren@foxmail.com>
2016-07-14 19:03:11 +0900

    feat+refactor: Add `image_clicked` view and refactor `index` view

--- annotator/views.py
+++ annotator/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.shortcuts import render
 from django.http import JsonResponse
 
@@ -6,12 +8,29 @@ from .utils import handle_uploaded_file
 
 # Create your views here.
 # One view is one page!
+# pwd is the main project directory!
 def index(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            result = handle_uploaded_file(request.FILES['file'])
+            f = request.FILES['file']
+            image_ext = f.name.split('.')[1]
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f")
+            path_pre = 'annotator/uploaded_images/'
+            image_path = path_pre + timestamp + '.' + image_ext
+            with open(image_path, 'wb+') as destination:
+                for chunk in f.chunks():
+                    destination.write(chunk)
+            result = handle_uploaded_file(image_path)
             return JsonResponse(result, safe=False)
     else:
         form = UploadFileForm()
     return render(request, 'annotator/index.html')
+
+
+def image_clicked(request):
+    image_path = request.POST['image_path']
+    # modify the path
+    image_path = 'annotator/static/' + image_path
+    result = handle_uploaded_file(image_path)
+    return JsonResponse(result, safe=False)    
